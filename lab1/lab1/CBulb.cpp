58887bab10a76445f85e07925bedcd6edd362718
alexv <alexvschulmann@gmail.com>
2018-09-09 21:16:03 -0700

    Seperated CBulb.h into header and implementation files

---
+++ lab1/lab1/CBulb.cpp
@@ -0,0 +1,53 @@
+#include "pch.h"
+#include "CBulb.h"
+
+CBulb::CBulb() {
+	state = 0;
+	watts = 0;
+	std::cout << "CBulb default constructor being called..." << std::endl;
+}
+
+CBulb::~CBulb() {
+	std::cout << "CBulb destructor being called..." << std::endl;
+}
+
+CBulb::CBulb(int initialWatts) : state(0), watts(initialWatts) {
+
+	std::cout << "CBulb constructor being called..." << std::endl;
+}
+
+void CBulb::print() {
+	if (state == 1) {
+		std::cout << "on" << std::endl;
+	}
+	else if (state == 0) {
+		std::cout << "off" << std::endl;
+	}
+
+}
+
+void CBulb::setwatts(int newWatts) {
+	watts = newWatts;
+}
+
+int CBulb::getstate() {
+	return state;
+}
+
+int CBulb::getpower() {
+	if (getstate()) {
+		return watts;
+	}
+	else {
+		return 0;
+	}
+
+}
+
+void CBulb::on() {
+	state = 1;
+}
+
+void CBulb::off() {
+	state = 0;
+}
--- lab1/lab1/CBulb.h
+++ lab1/lab1/CBulb.h
@@ -4,61 +4,21 @@
 #include <stdio.h>
 #include <string>
 
+
 class CBulb {
-	int state; // 0 = off, 1 = on
+	int state;
 	int watts;
 
 	public:
-		CBulb() {
-			state = 0;
-			watts = 0;
-			std::cout << "CBulb default constructor being called..." << std::endl;
-		}
-
-		~CBulb() {
-			std::cout << "CBulb destructor being called..." << std::endl;
-		}
-
-		CBulb(int initialWatts): state(0), watts(initialWatts){
-
-			std::cout << "CBulb constructor being called..." << std::endl;
-		}
-
-		void print() {
-			if (state == 1) {
-				std::cout << "on" << std::endl;
-			}
-			else if(state == 0){
-				std::cout << "off" << std::endl;
-			}
-			
-		}
-
-		void setwatts(int newWatts) {
-			watts = newWatts;
-		}
-
-		int getstate() {
-			return state;
-		}
-
-		int getpower() {
-			if (getstate()) {
-				return watts;
-			}
-			else {
-				return 0;
-			}
-			
-		}
-
-		void on() {
-			state = 1;
-		}
-
-		void off() {
-			state = 0;
-		}
+		CBulb();
+		~CBulb();
+		CBulb(int initialWatts);
+		void print();
+		void setwatts(int newWatts);
+		int getstate();
+		int getpower();
+		void on();
+		void off();
 };
 
 #endif 
