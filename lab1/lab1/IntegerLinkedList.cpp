3b14119678803997373aca0761536441154ab0e0
alexv <alexvschulmann@gmail.com>
2018-09-10 14:27:46 -0700

    Began set-up of LinkedList header and cpp files.

---
+++ lab1/lab1/IntegerLinkedList.cpp
@@ -0,0 +1,5 @@
+#include "pch.h"
+#include <stdio.h>
+#include <iostream>
+
+
---
+++ lab1/lab1/IntegerLinkedList.h
@@ -0,0 +1,17 @@
+#ifndef   __IntegerLinkedList__
+#define   __IntegerLinkedList__
+#include "IntegerLinkedList.h"
+
+class List
+{
+	public:
+		List();
+		~List();
+
+	private:
+
+
+};
+
+
+#endif
