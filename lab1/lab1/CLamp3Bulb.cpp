556c786a3666b94cf1951728ad9cc204170c6787
Alec Xu <alec_xu@hotmail.com>
2018-09-09 22:38:45 -0700

    added blank functions to CLamp3Bulb.cpp

--- lab1/lab1/CLamp3Bulb.cpp
+++ lab1/lab1/CLamp3Bulb.cpp
@@ -1,2 +1,31 @@
 #include "pch.h"
 #include "CLamp3Bulb.h"
+#include "CSwitch.h"
+
+CLamp3Bulb::CLamp3Bulb(int w1, int w2, int w3) {
+
+}
+
+CLamp3Bulb::CLamp3Bulb() {
+
+}
+
+void CLamp3Bulb::LampOn() {
+
+}
+
+void CLamp3Bulb::LampOff() {
+
+}
+
+void CLamp3Bulb::GetState() {
+
+}
+
+void CLamp3Bulb::Print() {
+
+}
+
+int GetPower() {
+
+}
